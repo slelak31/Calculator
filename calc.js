dc33478132c446bbde9de6da48944ef0cefcfeb1
Selmir Lelak <Lelaks@mail.gvsu.edu>
2025-11-21 01:12:21 -0500

    working display update with numbers that are input

--- calc.js
+++ calc.js
@@ -14,6 +14,7 @@ function divide (a, b) {
 let firstNum = undefined;
 let operator = undefined;
 let secondNum = undefined;
+let displayNum = '';
 
 function operate (oper, num1, num2) {
 
@@ -27,8 +28,45 @@ function operate (oper, num1, num2) {
         case '*':
             multiply(num1, num2);
             break;
-        case '/':
+        case '÷':
             divide(num1, num2);
             break;
     }
 }
+
+
+const buttons = document.querySelector(".buttons");
+const display = document.querySelector(".digits");
+
+buttons.addEventListener('click', (e) => {
+
+    switch (e.target.innerText)  {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            displayNum += e.target.innerText
+            display.textContent = displayNum;
+            break;
+        
+
+        case '+':
+            oper(e.target.innerText,)
+            break;
+        case '-':
+            console.log(add(6, 5));
+            break;
+        case '*':
+            console.log(add(7, 5));
+            break;
+        case '÷':
+            console.log(add(8, 5));
+            break;
+    }
+});
